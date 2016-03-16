b9abe6b1c4954b4ab73fe2f539b14c94ce509ff9
piscitellon <piscitellon@WITR90GMER4.wit.private>
2016-03-16 13:52:57 -0400

    Added documentation and project framework

---
+++ libraries_and_classes/Functions.cpp
@@ -0,0 +1,9 @@
+/*
+ * Functions.cpp
+ *
+ *  Created on: Mar 16, 2016
+ *      Author: piscitellon
+ */
+
+#include "Functions.h"
+
---
+++ libraries_and_classes/Functions.h
@@ -0,0 +1,14 @@
+/*
+ * Functions.h
+ *
+ *  Created on: Mar 16, 2016
+ *      Author: piscitellon
+ */
+
+#ifndef LIBRARIES_AND_CLASSES_FUNCTIONS_H_
+#define LIBRARIES_AND_CLASSES_FUNCTIONS_H_
+
+class Functions {
+};
+
+#endif /* LIBRARIES_AND_CLASSES_FUNCTIONS_H_ */
